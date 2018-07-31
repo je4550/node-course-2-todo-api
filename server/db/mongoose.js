20189c583fc6e8633ed024ad7a43b31a5ba948d0
Jeff Evans <Jeff.L.Evans@conocophillips.com>
2018-07-31 17:39:05 -0500

    fix bug

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://admin:test1234@ds159661.mlab.com:59661/todos);
+mongoose.connect('mongodb://admin:test1234@ds159661.mlab.com:59661/todos');
 
 module.exports = {
     mongoose
