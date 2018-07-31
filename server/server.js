70246366f4e34912283c85973e64ca147c9394cb
Jeff Evans <Jeff.L.Evans@conocophillips.com>
2018-07-31 17:47:33 -0500

    test

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
+    console.log(req.body);
     var todo = new Todo({
         text: req.body.text
     });
