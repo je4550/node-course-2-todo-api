792a554ccb137381100edde78abd4d5faff81ca5
Jeff Evans <Jeff.L.Evans@conocophillips.com>
2018-08-03 10:57:49 -0500

    add DELETE token

--- server/models/user.js
+++ server/models/user.js
@@ -70,6 +70,18 @@ UserSchema.statics.findByCredentials = function (email, password) {
     });
 };
 
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {
+                token: token
+            }
+        }
+    })
+}
+
 UserSchema.methods.toJSON = function () {
     var user = this;
     var userObject = user.toObject();
